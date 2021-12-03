4a480d20cc05068ec578441b98b46137f28ab0c4
Calvin Wang <calvinwang@yahoo.com>
2021-12-02 21:16:04 -0800

    request method

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/';
+// const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = '/api/';
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxODcxMzUzYTRkOWQ0MTdiZTQwMmM5YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjQyNzMwMywiZXhwIjoxNjM2Njg2NTAzfQ.dkaAjj87KkIi1jBYID1cWIcv3xigntFOCK2CKdKfT9Y'
 
 export const publicRequest = axios.create({
